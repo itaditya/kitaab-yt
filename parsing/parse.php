77c05497651e84e733b42a095ce2a792c6002182
Ankit Jain <ankitjain28may77@gmail.com>
2017-12-02 17:53:15 +0530

    Added parse file

--- parsing/parse.php
+++ parsing/parse.php
@@ -1,9 +1,26 @@
 <?php
 
-$service_url = "https://www.youtube.com/api/timedtext?expire=1512230718&v=0Y6s2kKpULU&caps=asr&hl=en_US&signature=7367116D83569F9A2138652600245F30788F50AA.C2E4BAFA13242EBC0B41C959C6D5EA89ED6004A4&sparams=asr_langs%2Ccaps%2Cv%2Cexpire&asr_langs=de%2Cko%2Cja%2Cnl%2Cen%2Cpt%2Ces%2Cru%2Cit%2Cfr&key=yttt1&kind=asr&lang=en&fmt=srv3";
+header("Access-Control-Allow-Origin: *");
+header('Access-Control-Allow-Methods: POST, GET, DELETE, PUT, PATCH, OPTIONS');
+header('Content-Type: application/json');
+// get the HTTP method, path and body of the request
+$method = $_SERVER['REQUEST_METHOD'];
+// $request = explode('/', trim($_SERVER['PATH_INFO'],'/'));
+
+if ($method!= 'POST') {
+    echo json_encode(['status' => 404]);
+    die();
+}
+$data = json_decode(file_get_contents('php://input'), true);
+var_dump($data);
+$service_url = $data['url'];
+$id = $data['id'];
+
+// $service_url = "https://www.youtube.com/api/timedtext?expire=1512230718&v=0Y6s2kKpULU&caps=asr&hl=en_US&signature=7367116D83569F9A2138652600245F30788F50AA.C2E4BAFA13242EBC0B41C959C6D5EA89ED6004A4&sparams=asr_langs%2Ccaps%2Cv%2Cexpire&asr_langs=de%2Cko%2Cja%2Cnl%2Cen%2Cpt%2Ces%2Cru%2Cit%2Cfr&key=yttt1&kind=asr&lang=en&fmt=srv3";
 
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, $service_url);
+$headers = ["Content-type" =>  "text/xml"];
 curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_HEADER, 0);
 
@@ -39,10 +56,11 @@ foreach ($trans as $fields) {
     $create .=  implode("\t", $fields);
     $create .= "\n";
 }
-if ($fp = fopen("Quick Easy Homemade Pizza Cooking Italian with Joe.tsv", 'w+')) {
+if ($fp = fopen($id.".tsv", 'w+')) {
     fwrite($fp, $create);
     fclose($fp);
 }
-echo $create;
+
+echo json_encode(["status" => 200]);
 
 ?>
