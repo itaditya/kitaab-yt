28f30d371ad3282c8864b51843e428d02e372c1c
itaditya <adityaa803@gmail.com>
2017-12-02 17:39:04 +0530

    progress

--- chromeExt/script.js
+++ chromeExt/script.js
@@ -1,29 +1,47 @@
-let toggle = false;
-chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  if (request.message === 'clicked_browser_action') {
-    toggle = !toggle;
-    if (toggle) {
-    } else {
-      init();
+console.log('test');
+function addXMLRequestCallback(callback) {
+  let oldSend, i;
+  if (XMLHttpRequest.callbacks) {
+    XMLHttpRequest.callbacks.push(callback);
+  } else {
+    XMLHttpRequest.callbacks = [callback];
+    oldSend = XMLHttpRequest.prototype.send;
+    XMLHttpRequest.prototype.send = function () {
+      for (i = 0; i < XMLHttpRequest.callbacks.length; i++) {
+        XMLHttpRequest.callbacks[i](this);
+      }
+      oldSend.apply(this, arguments);
+    }
+  }
+}
+
+addXMLRequestCallback(xhr => {
+  xhr.onload = function (e) {
+    if(xhr.responseURL.startsWith('https://www.youtube.com/api/timedtext')){
+      init(xhr.responseURL);
     }
   }
 });
 
-function init() {
-  console.log(window.location.href);
+function init(responseURL) {
   const videoId = window.location.search.substr(3);
-  sendVideoData({}, (data) => {
+  console.log(responseURL);
+  sendVideoData({
+    id: videoId,
+    url: responseURL
+  }, (data) => {
     console.log(data);
   })
 }
-// init();
+init();
 
 function sendVideoData(body, cb) {
-  fetch("http://127.0.0.1:8080/manifest.json", {
-      method: "GET",
+  fetch("https://49afc650.ngrok.io/parsing/parse.php", {
+      method: "POST",
       headers: {
         "Content-type": "application/json"
-      }
+      },
+      body
     })
     .then(res => {
       if (res.status !== 200) {
@@ -43,4 +61,3 @@ function sendVideoData(body, cb) {
       console.log(err);
     })
 }
-// http://127.0.0.1:8080/manifest.json
